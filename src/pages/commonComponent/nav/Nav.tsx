861c1ef0d8883bdaa235ba65bd2329e89216cdd8
sumit9722 <shahsumit9722@gmail.com>
2025-07-03 15:44:25 +0530

    Navigation and navbar

--- src/pages/commonComponent/nav/Nav.tsx
+++ src/pages/commonComponent/nav/Nav.tsx
@@ -1,8 +1,8 @@
 import styles from "./Nav.module.scss";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useEffect } from "react";
+import { useRef } from "react";
 
 const navItems = [
   { name: "Home", path: "/" },
@@ -14,29 +14,7 @@ const navItems = [
 ];
 
 export default function Nav() {
-  const location = useLocation();
-
-  useEffect(() => {
-    let lastScrollTop = window.pageYOffset;
-
-    const handleScroll = () => {
-      const currentScrollTop = window.pageYOffset;
-      const isScrollingDown = currentScrollTop > lastScrollTop;
-
-      const sidebarLines = document.querySelectorAll(`.${styles.sidebarline}`);
-      sidebarLines.forEach((line) => {
-        // line.style.backgroundColor = isScrollingDown ? "red" : "blue";
-      });
-
-      lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For mobile or negative scrolling
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const linesRef = useRef<HTMLDivElement[]>([]);
 
   useGSAP(() => {
     gsap.from(`.${styles.navItem}`, {
@@ -60,6 +38,9 @@ export default function Nav() {
       <div className={styles.sidebarlines}>
         {Array.from({ length: 5 }).map((_, index) => (
           <div
+            ref={(el) => {
+              if (el) linesRef.current[index] = el;
+            }}
             key={index}
             className={`${styles.sidebarline} ${
               styles[`sidebarline${index + 1}`]
